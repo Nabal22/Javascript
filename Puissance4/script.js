57e130f0f0853bf04f1ab59f293826cd1367e1c9
Nabal22 <alban.talagrand@gmail.com>
2022-03-28 22:29:19 +0200

    V 1

---
+++ Puissance4/script.js
@@ -0,0 +1,12 @@
+const coords = document.getElementById("coords");
+const plateau = document.getElementById("plateau");
+
+plateau.addEventListener('click',function(c){
+    coords.innerHTML ="x : "+c.clientX+ " y : "+c.clientY;
+    const jeton = document.elementFromPoint(c.clientX,c.clientY);
+    jeton.classList.add("jeton_rouge");
+});
+
+//Jeton 0=rouge 1=jaune -1= pas joué
+let ListePlateau = new Array(42);
+let compteurJ1,compteurJ2 = 0;
