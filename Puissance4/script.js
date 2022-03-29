bdfecb398a0d78bbe36f45fceea72854b16b3126
Alban Talagrand <alban.talagrand@etu.u-paris.fr>
2022-03-29 18:10:54 +0200

    V 0.5 changeplayer

--- Puissance4/script.js
+++ Puissance4/script.js
@@ -1,15 +1,60 @@
 const coords = document.getElementById("coords");
 const plateauElt = document.getElementById("plateau");
+
+const bStart = document.getElementById("start");
+
 const plateau = document.querySelectorAll(".jeton");
 
+let estfinie = false;
+
+let strTypeJeton = new String("");
+
+bStart.addEventListener('click',function(click){
+    bStart.style.display = "none" ;
+    estfinie = true;
+});
+
+
+
+function changePlayer(cmpt){
+    if (cmpt=1){
+        return 2;
+    }
+    else return 1;
+}
+
+let joueur=2;
+
 plateauElt.addEventListener('click',function(click){
-    let id;
-    for (let i = 0; i < plateau.length; i++) {
-        const element = plateau[i];
-        if(element===document.elementFromPoint(click.clientX,click.clientY)){
-            element.classList.add("jeton_rouge");
-            id = i;
+    let i;
+    //joueur = changePlayer(joueur);
+    if (joueur == 1){
+        strTypeJeton = "jeton_jaune";
+        joueur = 2;
+    }
+    else {
+        strTypeJeton ="jeton_rouge";
+        joueur = 1;
+    }
+    if (estfinie == false){
+        for (i = 0; i < plateau.length; i++) {
+            const element = plateau[i];
+            if(element===document.elementFromPoint(click.clientX,click.clientY)){
+                break;
+            }
         }
     }
-    //coords.innerHTML ="Client  : x : "+click.clientX+ " y : "+click.clientY+"\n"+"Index "+getIdFromClick(click.clientX,click.clientY);
+    else{
+
+    }
+    plateau[i].classList.add(strTypeJeton);
 });
+
+// premiere colonne
+
+        // plateau[0].classList.add("jeton_rouge");
+        // plateau[7].classList.add("jeton_rouge");
+        // plateau[14].classList.add("jeton_rouge");
+        // plateau[21].classList.add("jeton_rouge");
+        // plateau[28].classList.add("jeton_rouge");
+        // plateau[35].classList.add("jeton_rouge");
