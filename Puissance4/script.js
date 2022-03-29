c1b71ff5c1ed5dc5458d7e889716122f2068a9af
Alban Talagrand <alban.talagrand@etu.u-paris.fr>
2022-03-29 16:47:33 +0200

    V 0.3 : Récupération du jeton cliqué et de son id au clic

--- Puissance4/script.js
+++ Puissance4/script.js
@@ -2,26 +2,14 @@ const coords = document.getElementById("coords");
 const plateauElt = document.getElementById("plateau");
 const plateau = document.querySelectorAll(".jeton");
 
-
-plateauElt.addEventListener('click',function(c){
-    plateau[1].classList.add("jeton_rouge");
-
-    // Fonctionne bien
-    // Array.from(P).forEach((element, index) => 
-    // {
-    //     element.classList.add("jeton_rouge");
-    // });
-
-    // coords.innerHTML ="x : "+c.clientX+ " y : "+c.clientY;
-    // const jeton = document.elementFromPoint(c.clientX,c.clientY);
-    // jeton.classList.add("jeton_rouge");
-    // document.querySelectorAll("jeton");
+plateauElt.addEventListener('click',function(click){
+    let id;
+    for (let i = 0; i < plateau.length; i++) {
+        const element = plateau[i];
+        if(element===document.elementFromPoint(click.clientX,click.clientY)){
+            element.classList.add("jeton_rouge");
+            id = i;
+        }
+    }
+    //coords.innerHTML ="Client  : x : "+click.clientX+ " y : "+click.clientY+"\n"+"Index "+getIdFromClick(click.clientX,click.clientY);
 });
-
-
-// Parcourir element
-// const elements = document.querySelectorAll(".jeton");
-// Array.from(elements).forEach((element, index) => 
-// {
-// valeur += "Valeur " + element.name + ": " + element.value + "\r";
-// });
