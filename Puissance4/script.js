1fa678758d81b030896c4d446410aabd0bcbc63f
Alban Talagrand <alban.talagrand@etu.u-paris.fr>
2022-03-30 20:01:51 +0200

    V 0.5 Fonction associé au placement du jeton dans le bas de sa colonne

--- Puissance4/script.js
+++ Puissance4/script.js
@@ -1,7 +1,9 @@
 const coords = document.getElementById("coords");
 const plateauElt = document.getElementById("plateau");
+const auTourDe = document.getElementById("auTourDe");
 
-const bStart = document.getElementById("start");
+const bEnd = document.getElementById("end");
+const bRestart = document.getElementById("restart");
 
 const plateau = document.querySelectorAll(".jeton");
 
@@ -9,25 +11,29 @@ let estfinie = false;
 
 let strTypeJeton = new String("");
 
-bStart.addEventListener('click',function(click){
-    bStart.style.display = "none" ;
-    estfinie = true;
-});
-
-
-
-function changePlayer(cmpt){
-    if (cmpt=1){
-        return 2;
-    }
-    else return 1;
-}
+const c0 = [0, 7, 14, 21, 28, 35];
+const c1 = [1, 8, 15, 22, 29, 36];
+const c2 = [2, 9, 16, 23, 30, 37];
+const c3 = [3, 10, 17, 24, 31, 38];
+const c4 = [4, 11, 18, 25, 32, 39];
+const c5 = [5, 12, 19, 26, 33, 40];
+const c6 = [6, 13, 20, 27, 34, 41];
 
 let joueur=2;
 
-plateauElt.addEventListener('click',function(click){
-    let i;
-    //joueur = changePlayer(joueur);
+function restart(){
+    plateau.forEach(element => {
+        if(element.classList.contains("jeton_rouge")){
+            element.classList.remove("jeton_rouge");
+        }
+        else if (element.classList.contains("jeton_jaune")){
+            element.classList.remove("jeton_jaune");
+        }
+    });
+    estfinie = false;
+}
+
+function changePlayer(idJ) {
     if (joueur == 1){
         strTypeJeton = "jeton_jaune";
         joueur = 2;
@@ -36,10 +42,57 @@ plateauElt.addEventListener('click',function(click){
         strTypeJeton ="jeton_rouge";
         joueur = 1;
     }
-    if (estfinie == false){
+    return joueur,strTypeJeton;
+}
+
+function clicSurJetonEstValide(jeton) {
+    return (jeton.classList.contains("jeton") && !jeton.classList.contains("jeton_rouge") && !jeton.classList.contains("jeton_jaune"));
+}
+
+function estDejaJoue(jeton){
+    return (jeton.classList.contains("jeton_rouge") || jeton.classList.contains("jeton_jaune"));
+}
+
+bEnd.addEventListener('click',function(click){
+    bEnd.style.display = "none" ;
+    estfinie = true;
+});
+
+bRestart.addEventListener('click',()=>{
+    restart();
+    restart();
+    bEnd.style.display ="block";
+});
+
+function getColonne(i){
+    if(c0.includes(i)) return c0;
+    else if (c1.includes(i)) return c1;
+    else if (c2.includes(i)) return c2;
+    else if (c3.includes(i)) return c3;
+    else if (c4.includes(i)) return c4;
+    else if (c5.includes(i)) return c5;
+    else if (c6.includes(i)) return c6;
+}
+
+function placeInColonne(idC,typeJeton){
+    for (let index = 5; index >=0 ; index--) {
+        if(estDejaJoue(plateau[idC[index]])==false) {
+            plateau[idC[index]].classList.add(typeJeton);
+            break;
+        }
+    }
+}
+
+
+
+plateauElt.addEventListener('click',function(click){
+    let i;
+    const jeton = document.elementFromPoint(click.clientX,click.clientY);
+    if (estfinie == false && clicSurJetonEstValide(jeton)){
         for (i = 0; i < plateau.length; i++) {
-            const element = plateau[i];
-            if(element===document.elementFromPoint(click.clientX,click.clientY)){
+            if(plateau[i]===document.elementFromPoint(click.clientX,click.clientY)){
+                joueur,strTypeJeton = changePlayer(joueur);
+                placeInColonne(getColonne(i),strTypeJeton);
                 break;
             }
         }
@@ -47,7 +100,6 @@ plateauElt.addEventListener('click',function(click){
     else{
 
     }
-    plateau[i].classList.add(strTypeJeton);
 });
 
 // premiere colonne
